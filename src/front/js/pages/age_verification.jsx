6006a1de6770fe5d3d15f4808fd327ef48bc5ef9
MVCSystems <145077691+MVCSystems@users.noreply.github.com>
2024-09-12 19:39:54 -0500

    Revert "Add VC-7-HOME"

--- src/front/js/pages/age_verification.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const AgeVerification = () => {
-    return(
-        <div>
-            AgeVerification
-        </div>
-    )
-}
--- src/front/js/pages/genre_selection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const GenreSelection = () => {
-    return(
-        <div>
-            GenreSelection
-        </div>
-    )
-}
